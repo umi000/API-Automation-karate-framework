37e4db2db5f86d42e8de070b83e4d2fa5400a3ff
UMAIR ASLAM <UASLAM1000@Gmail.com>
2025-01-29 17:51:50 +0500

    Update UsersRunner.java

--- src/test/java/features/login/UsersRunner.java
+++ src/test/java/features/login/UsersRunner.java
@@ -7,6 +7,10 @@ class UsersRunner {
     @Karate.Test
     Karate testUsers() {
         return Karate.run("users").relativeTo(getClass());
-    }    
+    }
+    @Karate.Test
+    Karate testUsers2() {
+        return Karate.run("users").relativeTo(getClass());
+    } 
 
 }
